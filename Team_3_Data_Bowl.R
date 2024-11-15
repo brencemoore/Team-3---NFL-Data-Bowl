e444c4ff64f29d59833291db57cc320be48c848d
loganmcdavid11 <loganmcdavid21@gmail.com>
2024-11-15 14:01:20 -0600

    Updated knn method

--- Team_3_Data_Bowl.R
+++ Team_3_Data_Bowl.R
@@ -2,6 +2,8 @@
 
 #load tidyverse and plays.csv
 library(tidyverse)
+library(tidymodels)
+library(kknn)
 master_data <- read.csv("plays.csv")
 
 #Select only gameId, playID, quarter, down, yardsToGo, defensiveTeam, gameClock, pff_passCoverage, and pff_manZone
@@ -68,10 +70,62 @@ NO_data <- ready_master  |> filter(defensiveTeam == 'NO')
 CAR_data <- ready_master  |> filter(defensiveTeam == 'CAR')
 
 
+# We are looking to find how the defense performed based on all unique offensive plays ran 
 
+# Looking at what defensive alignment the defense goes into based on all of the variables in the test
+summary(CLE_data)
 
+# KNN is hwat I am going to try lol
+# Start with down and yards to go, seems simple enough right?
+# So in my head I am thinking x axis is down and y axis is yards until first down
+# Lets work with cleveland 
 
+# Getting a k value:
+#  We could do the sqrt(n) where n is the number of plays
 
+# Finding unique variables
+def_ids <- unique(CLE_data$pff_passCoverage)
+print(def_ids)
+
+# START: Split into training and testing 
+set.seed(123)
+data_split <- initial_split(CLE_data, prop = 0.7, strata = pff_passCoverage)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+# Define the model
+CLEknnClass <- nearest_neighbor(mode = "classification", neighbors = 9)
+
+# Define recipe
+CLEknnRecipe <- recipe(pff_passCoverage ~ down + yardsToGo, data = train_data) |>
+  step_normalize(all_numeric_predictors())
+
+# Assemble workflow
+CLEknnWflow <- workflow() |>
+  add_recipe(CLEknnRecipe) |>
+  add_model(CLEknnClass)
+
+# Fit the model
+CLEknnfit <- fit(CLEknnWflow, train_data)
+
+# Make predictions on test set
+testPred <- predict(CLEknnfit, test_data)
+
+# Ensure factor levels are the same for confusion matrix calculation
+common_levels <- union(levels(test_data$pff_passCoverage), levels(testPred$.pred_class))
+testPred <- testPred |>
+  mutate(
+    pff_passCoverage = factor(test_data$pff_passCoverage, levels = common_levels),
+    .pred_class = factor(.pred_class, levels = common_levels)
+  )
+
+# Print accuracy
+accuracy(testPred, truth = pff_passCoverage, estimate = .pred_class)
+
+# Print confusion matrix
+confusionMatrix <- testPred |>
+  conf_mat(truth = pff_passCoverage, estimate = .pred_class)
+confusionMatrix
 
 
 
